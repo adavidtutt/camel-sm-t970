8416904bf5ee78ac7d1dcbdfcc7f9435307800f8
David Tutt <adavidtutt@users.noreply.github.com>
2026-07-28 03:41:06 -0500

    Allow native Linux boot without Android SELinux policy

--- scripts/build-recovery.sh
+++ scripts/build-recovery.sh
@@ -13,7 +13,7 @@ partition_size=86888448
 stock_board=SRPTD21A007
 stock_os_version=11.0.0
 stock_os_patch_level=2024-08
-stock_cmdline="console=tty0 androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 rdinit=/init camel.sd_uuid=3963-3639"
+stock_cmdline="console=tty0 androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 selinux=0 rdinit=/init camel.sd_uuid=3963-3639"
 
 mkdir -p "$out_dir" "$work_dir"
 out_dir=$(realpath "$out_dir")
